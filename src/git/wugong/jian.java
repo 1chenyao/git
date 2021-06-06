4c33d94834a607f259dfa0e85f02c395b6988ca1
1chenyao <85423718+1chenyao@users.noreply.github.com>
2021-06-06 16:32:22 +0800

    Update jian.java

--- src/git/wugong/jian.java
+++ src/git/wugong/jian.java
@@ -4,7 +4,7 @@ public class jian {
 
 	public static void main(String[] args) {
 			System.out.println();
-			System.out.println("月aaaaa");
+			System.out.println("月aaaaa");System.out.println("月aaaaa");
 	}
 
 }
